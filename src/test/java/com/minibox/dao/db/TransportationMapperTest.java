27a591d5854869d54fc7f989ac41d2d53007ad7c
Mayyj <may.yjie@gmail.com>
2018-06-04 15:41:51 +0800

    测试

--- src/test/java/com/minibox/dao/db/TransportationMapperTest.java
+++ src/test/java/com/minibox/dao/db/TransportationMapperTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
@@ -16,6 +17,7 @@ import static org.junit.Assert.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@ActiveProfiles("dev")
 public class TransportationMapperTest {
 
     @Resource
